a216158bc41515374f14d62d9df7b68dee011f1b
betty Domain <betty.domain@free.fr>
2021-03-16 16:57:02 +0100

    add constructor to Patient entity and declare feign Client into RiskApplication

--- src/main/java/com/mediscreen/risk/RiskApplication.java
+++ src/main/java/com/mediscreen/risk/RiskApplication.java
@@ -2,8 +2,10 @@ package com.mediscreen.risk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.mediscreen.risk")
 public class RiskApplication {
 
     public static void main(String[] args) {
